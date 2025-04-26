f22972f0aa754de10ebb950d3863883e5593badb
kim-han-saem <khs_38@naver.com>
2025-04-26 21:40:05 +0900

    Add: Cascade Notation

--- main.dart
+++ main.dart
@@ -1,3 +1,26 @@
+class Player {
+  String name;
+  int xp;
+  String team;
+
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+  });
+
+  void sayHello() {
+    print('hello $name');
+  }
+}
+
 void main() {
-    print('hello world');
+  var han = Player(name: 'han', xp: 1200, team: 'red');
+  var potat = han
+    ..name = 'saem'
+    ..xp = 124
+    ..team = 'blue'
+    ..sayHello();
+
+  print(han.name);
 }
