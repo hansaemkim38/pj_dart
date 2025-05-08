d32b0e9954bb5be4ee3ea4073078103494d4fe9a
kim-han-saem <khs_38@naver.com>
2025-05-08 11:17:46 +0900

    feat: Abstract Classes

--- main.dart
+++ main.dart
@@ -1,7 +1,17 @@
-class Player {
-  String name;
-  int xp;
-  String team;
+abstract class Human {
+  void walk();
+}
+
+enum Team { red, blue }
+
+enum XpLevel { low, medium, high }
+
+enum Name { han, saem }
+
+class Player extends Human {
+  Name name;
+  XpLevel xp;
+  Team team;
 
   Player({
     required this.name,
@@ -12,14 +22,24 @@ class Player {
   void sayHello() {
     print('hello $name');
   }
+
+  void walk() {
+    print('im walking');
+  }
+}
+
+class Coach extends Human {
+  void walk() {
+    print('im coaching walking');
+  }
 }
 
 void main() {
-  var han = Player(name: 'han', xp: 1200, team: 'red');
-  var potat = han
-    ..name = 'saem'
-    ..xp = 124
-    ..team = 'blue'
+  var han = Player(name: Name.han, xp: XpLevel.low, team: Team.red);
+  var potatoGift = han
+    ..name = Name.saem
+    ..xp = XpLevel.high
+    ..team = Team.blue
     ..sayHello();
 
   print(han.name);
